993cfea06f42d9664be515c55593429562d15740
SaranyaAV <vensaranya2007@gmail.com>
2025-07-03 15:45:35 +0530

    Victory

--- script.js
+++
@@ -1,8 +0,0 @@
-
-const bookmarks = [
-  "India's Space Mission Success",
-  "New Tech in AI Changing the World"
-];
-
-const bookmarkSection = document.getElementById("bookmarks");
-bookmarkSection.innerHTML = "<ul>" + bookmarks.map(item => `<li>${item}</li>`).join('') + "</ul>";
